19c287bf80c64bac513cac353bdb9d40bd4965d7
ceberous <cerbus.collin@gmail.com>
2018-08-21 15:40:39 -0400

    57

--- server/utils/cecClientManager.js
+++ server/utils/cecClientManager.js
@@ -1,4 +1,4 @@
-require('shelljs/global');
+require( "shelljs/global" );
 module.exports.activate =  function() {
 	exec( "echo 'on 0' | cec-client -s -d 1" , { silent:true , async: false } );
 	setTimeout( function() {
@@ -6,7 +6,10 @@ module.exports.activate =  function() {
 	} , 2000 );
 	setTimeout( function() {
 		exec( "echo 'as' | cec-client -s -d 1" , { silent:true , async: false } );
-	} , 5000 );	
+            setTimeout( function() {
+				exec( "export DISPLAY=:0.0; xrandr --output eDP1 --off --output HDMI1 --primary --mode 1920x1080" , { silent: true , async: false } );
+           	} , 1000 );
+	} , 3000 );	
 };
 module.exports.turnOff = function() {
 	exec( "echo 'standby 0' | cec-client -s -d 1" , { silent:true , async: false } );
